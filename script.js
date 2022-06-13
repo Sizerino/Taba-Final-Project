ac3b1f1ff7d8d672f81d0fc60321245453a87221
Sizerino <myultisreadylol@gmail.com>
2022-06-13 09:20:04 +0300

    still trying to make this work

--- script.js
+++ script.js
@@ -50,6 +50,43 @@ BackToTop = () => {
     window.scrollTo({top: 0, behavior: "smooth"});
 }
 
+// rememberButton = (btn) => {
+//     if ($(".vote").hasClass("btn-success")) {
+//         console.log('reached if');
+    
+//         localStorage.setItem('clickedButton', btn);
+//         $(btn).removeClass("btn-success");
+//         $(btn).addClass("btn-warning");
+//     }
+//     else if ($(".vote").hasClass("btn-warning")) {
+//         console.log('reached else if');
+
+//         localStorage.setItem("clickedButton", btn);
+//         $(btn).removeClass("btn-warning");
+//         $(btn).addClass("btn-success");
+//     }
+// }
+
+// rememberButton = (btn) => {
+//     console.log("reached start")
+//     $(btn).click(function() {
+//         console.log("reached mid")
+//         if ($(btn).hasClass("btn-success")) {
+//             console.log('reached if');
+        
+//             localStorage.removeItem('clickedButton', btn);
+//             $(this).removeClass("btn-success");
+//             $(this).addClass("btn-warning");
+//         }
+//         else if ($(btn).hasClass("btn-warning")) {
+//             console.log('reached else if');
+    
+//             localStorage.setItem("clickedButton", btn);
+//             $(this).removeClass("btn-warning");
+//             $(this).addClass("btn-success");
+//         }
+//     });
+// }
 
 
 window.onload = () => {
@@ -57,7 +94,6 @@ window.onload = () => {
         if ($(".vote").hasClass("btn-success")) {
             console.log('reached if');
 
-            $(this).removeClass("ss");
             localStorage.setItem('clicked', 'no');
             $(this).removeClass("btn-success");
             $(this).addClass("btn-warning");
@@ -65,19 +101,17 @@ window.onload = () => {
         else if ($(".vote").hasClass("btn-warning")) {
             console.log('reached else if');
 
-            $(this).addClass("ss");
             localStorage.setItem('clicked', 'yes');
             $(this).removeClass("btn-warning");
             $(this).addClass("btn-success");
         }
     });
-    $(document).on('click', '.exit', function () {
-        if (localStorage.getItem('clicked') === 'yes') {
-            console.log('reached localstorage');
-    
-            $("ss").removeClass("btn-warning");
-            $("ss").addClass("btn-success");
-        }
-    });
-    
+    if (localStorage.getItem('clicked') === 'yes') {
+        console.log('reached localstorage 1');
+
+        $(".ss").removeClass("btn-warning");
+        $(".ss").addClass("btn-success");
+    } else {
+        alert("error");
+    }
 }
